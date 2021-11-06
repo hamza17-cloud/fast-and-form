87205d713e37674c1fe9eaa8efd4afd6780d0953
TheRolf <yann.lv@live.fr>
2021-11-06 15:02:46 +0100

    Initial commit

---
+++ src/test/java/fr/utbm/da50/fastandform/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.utbm.da50.fastandform.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
