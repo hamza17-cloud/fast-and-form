eababe02a5005af2cebe33427047a818cc462686
Hassan <hachichou.hassan@gmail.com>
2021-12-19 07:30:00 +0100

    find mongo 01

--- src/main/java/fr/utbm/da50/fastandform/core/controller/FileController.java
+++ src/main/java/fr/utbm/da50/fastandform/core/controller/FileController.java
@@ -1,8 +1,8 @@
 package fr.utbm.da50.fastandform.core.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.utbm.da50.fastandform.core.repository.GeneralRepository;
@@ -18,8 +18,16 @@ class FileController {
   @Autowired
   private GeneralRepository generalRepository;
 
-  @GetMapping(value="/test", produces = MediaType.APPLICATION_JSON_VALUE)
-  String test() {
-    return generalRepository.allDocuments().toString();
-  }
+  // exemple : http://localhost:8080/fastandform/users
+  @GetMapping(value="/{DatabaseName}/{CollectionName}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public String findAll(@PathVariable String DatabaseName, @PathVariable String CollectionName) {
+
+		return generalRepository.findAllDocuments(DatabaseName, CollectionName).toString();
+	}
+  // exemple : http://localhost:8080/fastandform/users/207471947662098432
+  @GetMapping(value="/{DatabaseName}/{CollectionName}/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public String findById(@PathVariable String DatabaseName, @PathVariable String CollectionName, @PathVariable String id) {
+
+		return generalRepository.findOneDocumentById(DatabaseName, CollectionName, id).toString();
+	}
 }
--- src/main/java/fr/utbm/da50/fastandform/core/repository/GeneralRepository.java
+++ src/main/java/fr/utbm/da50/fastandform/core/repository/GeneralRepository.java
@@ -5,23 +5,29 @@ import java.util.List;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
-
+import static com.mongodb.client.model.Filters.*;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class GeneralRepository {
-  private static final String DatabaseName = "fastandform";
-  private static final String CollectionName = "users";
+ // private static final String DatabaseName = "fastandform";
+  //private static final String CollectionName = "users";
 
   @Autowired
   private MongoClient client;
 
-  public List<String> allDocuments() {
-      final List<String> list = new ArrayList<>();
-      final MongoCollection<Document> data = client.getDatabase(DatabaseName).getCollection(CollectionName);
-      data.find().map(Document::toJson).forEach(list::add);
-      return list;
-  }
+  public List<String> findAllDocuments(String DatabaseName, String CollectionName) {
+    final List<String> list = new ArrayList<>();
+    final MongoCollection<Document> data = client.getDatabase(DatabaseName).getCollection(CollectionName);
+    data.find().map(Document::toJson).forEach(list::add);
+    return list;
+}
+
+  public String findOneDocumentById(String DatabaseName, String CollectionName, String id) {
+    final MongoCollection<Document> data = client.getDatabase(DatabaseName).getCollection(CollectionName);
+    Document myDoc = data.find(eq("id", id)).first();
+    return myDoc.toJson();
+}
 }
